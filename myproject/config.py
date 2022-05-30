7c40d5db24aa604c10b1191a00ac30a07e9e1a5b
Daniel Riggs <daniel.riggs1@gmail.com>
2022-05-30 16:29:50 -0400

    Add postgres in docker compose

--- myproject/config.py
+++ myproject/config.py
@@ -7,3 +7,7 @@ config = Config(Path("./").parent / ".env")
 DEBUG = config.get("DEBUG", cast=bool)
 HOST = config.get("HOST", default="127.0.0.1")
 PORT = config.get("PORT", cast=int, default="8000")
+
+DB_NAME = config.get("DB_NAME", default="recordings")
+DB_USER = config.get("DB_USER")
+DB_PASSWORD = config.get("DB_PASSWORD")
