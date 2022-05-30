c9d2cab82901c164c4df9b4dd0f3de88483effd9
Daniel Riggs <daniel.riggs1@gmail.com>
2022-05-30 11:34:47 -0400

    First commit, see if black hook is working

---
+++ main.py
@@ -0,0 +1,16 @@
+# from fastapi import FastAPI
+#
+#
+# app = FastAPI()
+#
+#
+# @app.get("/")
+# async def home():
+#     return {"message": "Welcome Home"}
+def main() -> str:
+
+    return 0
+
+
+if __name__ == "__main__":
+    main()
