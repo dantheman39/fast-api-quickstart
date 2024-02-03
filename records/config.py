76ceb94dbfadb0535932ab603b763edc443796b5
Daniel Riggs <daniel.riggs1@gmail.com>
2024-02-03 18:24:16 -0500

    Some cleanup on default values for environment variables

--- records/config.py
+++ records/config.py
@@ -4,13 +4,13 @@ from starlette.config import Config
 
 
 config = Config(Path("./").parent / ".env")
-DEBUG = config.get("DEBUG", cast=bool)
+DEBUG = config.get("DEBUG", cast=bool, default=False)
 HOST = config.get("HOST", default="127.0.0.1")
 PORT = config.get("PORT", cast=int, default="8000")
 ENV = config.get("ENV", default="dev")
 
 test_postfix = "_test" if ENV.lower() == "test" else ""
-DB_NAME = config.get("DB_NAME", default="recordings") + test_postfix
+DB_NAME = config.get("DB_NAME", default="records") + test_postfix
 DB_USER = config.get("DB_USER")
 DB_PASSWORD = config.get("DB_PASSWORD")
 # Default is the service name in docker compose
